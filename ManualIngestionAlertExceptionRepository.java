b56ebc124ae01a6570d5431d74a6dfa1a55dac3a
Vaibhav Kumar <vaibhavvirat18@gmail.com>
2026-06-13 09:36:19 +0530

    changes to be pushed

--- ManualIngestionAlertExceptionRepository.java
+++ ManualIngestionAlertExceptionRepository.java
@@ -26,6 +26,9 @@ public class ManualIngestionAlertException {
     @Column(name = "reason")
     private String reason;
 
+    @Column(name = "record_date")
+    private String recordDate;
+
     @Column(name = "date_of_attestation")
     private String dateOfAttestation;
 
